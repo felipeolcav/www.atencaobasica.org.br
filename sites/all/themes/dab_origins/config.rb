c1676cdd7ac6c7c4fc1bbb7f9001a558ea1c20b5
Pedro Rocha <pedrorocha@singleview.com.br>
2013-08-24 11:43:25 -0300

    Melhorias de layout e usabilidade gerais para o site, padronizando algumas partes e contextualizando os node_forms

---
+++ sites/all/themes/dab_origins/config.rb
@@ -0,0 +1,11 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :expanded
+# output_style = :compressed
+
+relative_assets = true
